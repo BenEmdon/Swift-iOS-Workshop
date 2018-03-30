15be601aaefeaf38d8005d6f3b7a4dbdb51c13b3
BenEmdon <benjaminemdon@gmail.com>
2018-03-29 20:18:09 -0400

    Started Tour of Swift playground

---
+++ Tour of Swift.playground/Pages/WelcomePage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+# Welcome to the Tour of Swift!
+This is a Swift Playground where "people come to play".
+
+Swift Playgrounds are part of the Xcode Integrated Development Enviroment (IDE). What makes them special is that the code you write is compiled after every change, so you can see the results quickly.
+
+Swift Playgrounds are the perfect place to play, learn, and prototype code.
+
+*/
+
+// Lets start by printing "Hello World!".
+// In Swift this can be done in one line:
+print("Hello World!")
+
+//: [👉 continue the tour](@next)
