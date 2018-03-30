3dc6c89e55748f206bc473e966de14cc25eb7598
BenEmdon <benjaminemdon@gmail.com>
2018-03-30 13:35:51 -0400

    Created some new pages/topics

---
+++ Tour of Swift.playground/Pages/Unit Converter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+# Currency Converter
+Now we will begin to build a simple/small unit converter. Here we will learn about `enums`.
+*/
+
+// Swift Enums are super powerful because you can define functions on their type:
+enum DigitalStorage: Int {
+	case bit = 1
+	case byte = 8
+	case kilobit = 1000
+	case kilobyte = 8000
+	case megabit = 1_000_000
+	case megabyte = 8_000_000
+	case gigabit = 1_000_000_000
+	case gigabyte = 8_000_000_000
+
+
+	func convert(amount: Float, to unit: DigitalStorage) -> Float {
+		return amount * Float(self.rawValue) / Float(unit.rawValue)
+	}
+}
+
+// Here is how you would we would do some conversions:
+DigitalStorage.gigabit.convert(amount: 2, to: .bit)
+DigitalStorage.gigabyte.convert(amount: 4, to: .gigabit)
+DigitalStorage.gigabyte.convert(amount: 5, to: .byte)
+
+//: [Next](@next)
