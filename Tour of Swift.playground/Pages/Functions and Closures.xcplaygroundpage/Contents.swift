863f9ded0da2dc5f23b3b092f9453407d17bcac1
BenEmdon <benjaminemdon@gmail.com>
2018-03-30 13:36:57 -0400

    Added the Functions and Closures topic

--- Tour of Swift.playground/Pages/Functions and Closures.xcplaygroundpage/Contents.swift
+++ Tour of Swift.playground/Pages/Functions and Closures.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
+/*:
+# Functions and Closures
+*/
 
-import Foundation
+// Use keyword `func` to start defining a fuction:
+func factorial(number: Int) -> Int {
+	if number <= 1 {
+		return 1
+	} else {
+		return number * factorial(number: number - 1)
+	}
+}
 
-var str = "Hello, playground"
+// Invoking a function:
+factorial(number: 1)
+factorial(number: 2)
+factorial(number: 3)
+factorial(number: 4)
+
+// Since functions are closures, they can be passed around like other values.
+let factorialFunction = factorial
+factorialFunction(4)
 
 //: [Next](@next)
